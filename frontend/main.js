d044eec0327a8a9a1fec54bca7861c300499192b
unknown <milla_russell@hotmail.com>
2020-10-04 18:22:49 -0300

    mais letras

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,20 @@
 let sequencia = "";
 
 function enter() {
-    axios.get(`http:localhost:3000/${sequencia}`).then((resposta)=>{alert(resposta.data)});
+  axios.get(`http:localhost:3000/${sequencia}`).then((resposta) => {
+    const display = document.querySelector("#display");
+    display.value = resposta.data;
+  });
 }
 
 function clicar(numero) {
-    sequencia += numero
-    console.log(sequencia)
+  sequencia += numero;
+  const display = document.querySelector("#display");
+  display.value = sequencia;
 }
 
 
-
+function resetar(){
+    const display = document.querySelector("#display");
+    display.value = "";
+}
